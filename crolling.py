2afe308e649b4ab31afdbf790839ca8c54a8ccc3
min <giminug593@gmail.com>
2023-08-01 14:19:04 +0700

    update

--- crolling.py
+++ crolling.py
@@ -1,40 +1,37 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import time
 import requests
-import re 
+from bs4 import BeautifulSoup
 
-url = "https://www.yes24.com/24/category/bestseller"
-
-driver = webdriver.Chrome()
-driver.get(url)
-soup = BeautifulSoup(driver.page_source,"lxml")
+base_url = "https://www.yes24.com/24/category/bestseller?CategoryNumber=001&sumgb=06&fetchSize=40&PageNumber={}"
 
 
+for n in range(1, 26):
+    url = base_url.format(n)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-title = soup.find_all("li",attrs={"class":re.compile("^num")})
-images = soup.find_all("p", attrs={"class": "image"})
+    table = soup.find_all("td", attrs={'class': 'goodsTxtInfo'})
+    imgs = soup.find_all("div", attrs={'class': 'goodsImgW'})
 
+    for row,img in zip(table,imgs):
+        p = row.select_one('p > a')
+        element = row.select_one('div.aupu > a[target=_blank]')
+        m = row.select_one('span.priceB')
+        img_url = img.find('img')['src']
+        print('URL:', img_url)
+        print('제목:', p.get_text())
+        if element:
+            text = element.text
+            print('작가:', text)
+        else:
+            div_element = row.select_one('div.aupu')
+            if div_element:
+                text = div_element.text
+                print('작가:', text)
+            
+        print('가격:', m.get_text())
+    
+    
 
         
-for idx,titles in enumerate(title):
-    
-    a = titles.find("p",class_=False).get_text()
-    b = titles.find("a",attrs={"target":"_blank"}).get_text()
-    p = titles.find("strong").get_text()
-    img_tag = titles.find("img")
-    if img_tag and "src" in img_tag.attrs:
-            img_url = img_tag["src"]
-            response = requests.get(img_url)
-            if response.status_code == 200:
-                
-                with open(f"movie{idx+1}.jpg", "wb") as f:
-                    f.write(response.content)
-                if idx >= 40 : 
-                    break 
-                print(f"Saved image {idx+1}")
-            else:
-                print(f"Failed to retrieve image {idx+1}")
-    print("제목:",a)
-    print("작가:",b)
-    print("가격:",p)
-    
+        
